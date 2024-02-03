b66d072b1004e090c6752bf03889c5556bb556c3
荣文 <498350397@qq.com>
2024-02-04 07:52:40 +0800

    commit

--- src/main/java/com/rongwen/zuo/test/Test03.java
+++ src/main/java/com/rongwen/zuo/test/Test03.java
@@ -21,7 +21,42 @@ public class Test03 {
      * 和为16
      */
 
+    public int smallSum(int[] arr) {
+        if (arr == null || arr.length < 2) return 0;
+        return process(arr, 0, arr.length - 1);
+    }
 
+    public int process(int[] arr, int L, int R) {
+        if (L == R) return 0;
+        int mid = L + ((R - L) >> 1);
+        return process(arr, L, mid) + process(arr, mid + 1, R) + merge(arr, L, mid, R);
+    }
 
+    public int merge(int[] arr, int L, int mid, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = mid + 1;
+        int res = 0;
+        while (p1 <= mid && p2 <= R) {
+            res += arr[p1] < arr[p2] ? (R - p2 + 1) * arr[p1] : 0;
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,5};
+        System.out.println(new Test03().smallSum(arr));
+    }
 
 }
