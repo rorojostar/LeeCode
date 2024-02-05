2037f2a61db3ebd57c76706e5e6eed9aa2904eac
荣文 <498350397@qq.com>
2024-02-05 19:18:46 +0800

    commit

--- src/main/java/com/rongwen/top100/doublePoint/ThreeNumsSum.java
+++ src/main/java/com/rongwen/top100/doublePoint/ThreeNumsSum.java
@@ -1,4 +1,46 @@
 package com.rongwen.top100.doublePoint;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ThreeNumsSum {
+
+    /**
+     * middle
+     * 15. 三数之和
+     * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+     * 你返回所有和为 0 且不重复的三元组。
+     * 注意：答案中不可以包含重复的三元组。
+     * 输入：nums = [-1,0,1,2,-1,-4]
+     * 输出：[[-1,-1,2],[-1,0,1]]
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 3) return res;
+        Arrays.sort(nums); //排序
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break; //第一个元素大于0.不用继续了
+            if (i > 0 && nums[i] == nums[i - 1]) continue; //去重
+            int L = i + 1, R = nums.length - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++;
+                    while (L < R && nums[R] == nums[R - 1]) R--;
+                    L++;
+                    R--;
+                } else if (sum > 0) {
+                    R--;
+                } else {
+                    L++;
+                }
+            }
+        }
+        return res;
+    }
 }
