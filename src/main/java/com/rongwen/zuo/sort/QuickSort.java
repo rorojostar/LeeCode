d66532e49a0e89e9afeeafa0065a2a790655ee52
荣文 <498350397@qq.com>
2024-02-05 08:37:59 +0800

    commit

---
+++ src/main/java/com/rongwen/zuo/sort/QuickSort.java
@@ -0,0 +1,11 @@
+package com.rongwen.zuo.sort;
+
+public class QuickSort {
+
+    public void quickSort1(){
+
+
+    }
+
+
+}
---
+++ src/main/java/com/rongwen/zuo/test/Test05.java
@@ -0,0 +1,15 @@
+package com.rongwen.zuo.test;
+
+public class Test05 {
+    /**
+     * q1:给定一个数组arr，和一个数num,请把小于等于num的数放在数组的左边，大于num的数放在数组的右边，O(N),O(1)
+     * q2荷兰国旗问题:给定一个数组arr，和一个数num,请把小于num的数放在数组的左边，等于num的数放在中间，大于num的数放在数组的右边
+     */
+
+    public void q1(int[] arr, int num) {
+
+
+    }
+
+
+}
